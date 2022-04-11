679ed7ae91a06443e79f70dc4f83e474418fc524
Lucas_Pavlovici <lupavlo3399@gmail.com>
2022-04-11 19:17:09 +0200

    0.1.5

--- group_project/p2Admin.php
+++ group_project/p2Admin.php
@@ -13,16 +13,16 @@
         <div class="container">
             <div class="screen">
                 <div class="screen__content">
-                    <form class="login">
+                    <form method="POST" action ="" class="login">
                         <div class="login__field">
                             <i class="login__icon fas fa-user"></i>
-                            <input type="text" class="login__input" placeholder="User name / Email">
+                            <input type="text" class="login__input" placeholder="Admin ID" name="ID">
                         </div>
                         <div class="login__field">
                             <i class="login__icon fas fa-lock"></i>
-                            <input type="password" class="login__input" placeholder="Password">
+                            <input type="password" class="login__input" placeholder="Password" name="passwd">
                         </div>
-                        <button class="button login__submit">
+                        <button class="button login__submit" name="submit">
                             <span class="button__text">Log In Now</span>
                             <i class="button__icon fas fa-chevron-right"></i>
                         </button>				
@@ -46,3 +46,28 @@
 
     </body>
 </html>
+
+<?php
+session_start();
+$database = new PDO('mysql:host=localhost;dbname=groupproject', 'root', '');
+$allAdmins = $database->query('SELECT * FROM admin');
+
+if(isset($_POST['submit'])){
+    if(!empty($_POST['ID']) AND !empty($_POST['passwd'])){
+
+        $input_ID = htmlspecialchars($_POST['ID']);
+        $input_passwd = htmlspecialchars($_POST['passwd']);
+        
+        while($admin = $allAdmins->fetch()){
+            if($input_ID == $admin['AdminID'] AND $input_passwd == $admin['Password']){
+                $_SESSION['passwd'] = $input_passwd;
+                header('Location: p3Admin.php');
+            }else{
+                echo "Your user name or password is incorrect";
+            }
+        }
+    }else{
+        echo "Please complete the fields";
+    }
+}
+?>
