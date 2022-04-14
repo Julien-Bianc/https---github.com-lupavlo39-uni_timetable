c63d98eacfa8f5851ac57eebafe06366cab4c015
Lucas_Pavlovici <lupavlo3399@gmail.com>
2022-04-15 01:30:16 +0200

    test timetable

--- group_project/timetable.php
+++ group_project/timetable.php
@@ -1,35 +1,43 @@
-<body>
-    <article>
-        <table border="2" style= "background-color: #FFFFFF; color: #000000; margin: 0 auto;" >
-              <tr>
-                <th><b>Monday</b></th>
-                <th><b>Tuesday</b></th>
-                <th><b>Wednesday</b></th>
-                <th><b>Thursday</b></th>
-                <th><b>Friday</b></th>
-              </tr>
-            <tbody>
-                <h1>Class Wise Time Table</h1>
-                <nav>
-                    <a href="index.php">Home</a>
-                </nav>
-                <?php/*
-                    $days_data = array();
-                    while($row = mysqli_fetch_assoc($res)){
-                            $days_data[$row['day']] == $row;
-                    }
-
-                    if($days_data):
-                        $week_days = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
-                        foreach($week_days as $wday){
-                            if(!empty($days_data[$wday])){
-                                $row_data = $days_data[$wday]; // $row_data contains all of the data from that specific day time,name,teacher and etc...
-                                // there is data for this day
-                            } else {
-                                // no data for the day currently in loop
-                            }
-                        }
-                    endif;        
-                */?>
-                </tbody>
-            </table>
+<table border="2" style= "background-color: #84ed86; color: #761a9b; margin: 0 auto;" >
+    <tr>
+        <th><b>TIME</b></th>
+        <th><b>Monday</b></th>
+        <th><b>Tuesday</b></th>
+        <th><b>Wednesday</b></th>
+        <th><b>Thursday</b></th>
+        <th><b>Friday</b></th>
+    </tr>
+    <tbody>
+    <?php
+        error_reporting(1);
+        session_start();
+        if ((isset($_SESSION['passwd'])) && (isset($_REQUEST['cls']))) {
+            $classname = $_REQUEST['cls'];
+            $conn = mysqli_connect("localhost", "root", "", "groupproject");
+            $sql = "select DISTINCT (hour) as hour from modules where ModuleName = "."'$classname'";
+            $rest = mysqli_query($conn, $sql);
+            while($t = mysqli_fetch_assoc($rest)){
+            $sql = "select * from modules where ModuleName = "."'$classname'". " AND hour = '".$t['hour']."'";
+            //print_r($sql);exit;
+            $res = mysqli_query($conn, $sql);
+    ?>
+    <tr>
+        <td><?php echo $t['hour'] ?></td>
+        <?php
+                $week_days = array('Monday','Tuesday','wednesday','Thursday','Friday');
+                $classes = array();
+                while($row = mysqli_fetch_assoc($res)) {
+                    $classes[$row['day']] = $row;
+                }
+                foreach ($week_days as $day) {
+        ?>
+        <?php if (array_key_exists($day, $classes)) { $row = $classes[$day]; ?>
+        <td><?php echo $row['IDModule'] . '<br />' . $row['NumRoom'] ?></td>
+        <?php } else { ?>
+        <td>No Class</td>
+        <?php } ?>
+        <?php } ?>
+    </tr>
+    <?php } ?>
+    </tbody>
+</table>
